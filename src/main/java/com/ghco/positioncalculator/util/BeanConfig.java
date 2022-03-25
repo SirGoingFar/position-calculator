0bb0309cafbf72a3e11123e15b8b21a54d83e13d
Akintunde <akintunde_olanrewaju@yahoo.co.uk>
2022-03-25 14:53:19 +0200

    Cover for dataset use for either main application or test execution

--- src/main/java/com/ghco/positioncalculator/util/BeanConfig.java
+++ src/main/java/com/ghco/positioncalculator/util/BeanConfig.java
@@ -4,15 +4,25 @@ import com.ghco.positioncalculator.repository.CsvBackedTradeRepository;
 import com.ghco.positioncalculator.repository.TradeRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+
+import static com.ghco.positioncalculator.util.Constants.IS_TEST;
 
 @Configuration
 public class BeanConfig {
 
     @Bean
-    public TradeRepository csvBasedTradeRepository() {
-        return new CsvBackedTradeRepository(new File("/Users/Akintunde/Documents/Software_Engineering/Learning/Backend/Framework/Spring_Boot/Mine/position-calculator/src/main/resources/static/trades.xlsx"));
+    public TradeRepository csvBasedTradeRepository() throws FileNotFoundException {
+        File tradeDataFile = ResourceUtils.getFile("classpath:static/trades.xlsx");
+
+        if(IS_TEST){
+            tradeDataFile = ResourceUtils.getFile("classpath:static/trades-test-data.xlsx");
+        }
+
+        return new CsvBackedTradeRepository(tradeDataFile);
     }
 
 }
---
+++ src/main/java/com/ghco/positioncalculator/util/Constants.java
@@ -0,0 +1,5 @@
+package com.ghco.positioncalculator.util;
+
+public class Constants {
+    public static final boolean IS_TEST = false;
+}
